ccec57277230cc2d33a17058baad87b8b2a7b171
Эдуард Миловский <66972269+Meyronn@users.noreply.github.com>
2022-07-14 22:11:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ let tg = window.Telegram.WebApp;
 let data = tg.initDataUnsafe;
 
 document.querySelector('#title').innerText = `Hi, ${data.user.first_name}!`;
-document.querySelector('#paragraf').innerText = `.config -> bio_url -> Enter value`;
